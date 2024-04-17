6a5c492903ac3923aed10f38c5f760a86f03ac4f
Dustin Fang <20fangd@gmail.com>
2024-04-17 16:51:41 -0400

    init swift proj

---
+++ todo/todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Dustin Fang on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
